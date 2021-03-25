ff02c9d1484146cec59f644104a95e40ee53ef1d
ryujioda <ryuji.oda@gmail.com>
2021-03-26 08:52:03 +0900

    add: bind viewModel into Fragment

--- app/src/main/java/com/example/jetpackprogramming_fragment/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/jetpackprogramming_fragment/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.jetpackprogramming_fragment.R
+import com.example.jetpackprogramming_fragment.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,18 +17,22 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = //inflateでViewインスタンスを作成
-        inflater.inflate(R.layout.main_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.lifecycleOwner = this //ライブデータ更新などをこのフラグメントで受けられるようにする仕組み
+        return binding.root
+    }
 
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
     }
 
 }
--- app/src/main/java/com/example/jetpackprogramming_fragment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/jetpackprogramming_fragment/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package com.example.jetpackprogramming_fragment.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var message = "MainFrgment!"
 }
